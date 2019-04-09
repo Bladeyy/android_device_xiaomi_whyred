e42311a2116dc02c9f635167d67af6fb3bad75c8
Bladeyy <getsuga369@gmail.com>
2019-04-09 05:43:42 +0000

    whyred: Revert back to 9.5.11 fingerprint

--- revengeos_whyred.mk
+++ revengeos_whyred.mk
@@ -41,9 +41,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 9.4.2 release-keys"
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:9/PKQ1.180904.001/9.4.2:user/release-keys
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
 
 TARGET_VENDOR := Xiaomi
